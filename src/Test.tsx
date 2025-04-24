e985f42dc5bd01b14b0001362648855de0e084fd
Mario Burali <marioburali@hotmail.com>
2025-04-24 18:35:25 -0300

    a

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+
 function Test() {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <div>
       <h1>Test</h1>
       <p>This is a test component.</p>
+      <button onClick={() => setClicked(true)}>Click here</button>
+
+      {clicked && (
+        <div>
+          <h1>You made it!</h1>
+          <p>Your component is working!</p>
+        </div>
+      )}
     </div>
   );
 }
