605277f6ba90aec6605513ae6803e7703e2c89ed
Mario Burali <marioburali@hotmail.com>
2025-04-24 18:22:32 -0300

    test3

--- src/App.tsx
+++ src/App.tsx
@@ -24,9 +24,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+          teste
+        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more...
+        Click on the Vite and React logos to learn more.
       </p>
     </>
   )
